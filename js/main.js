c09a612c541fee1b02febf8400ba818fba0450cd
Yaoyu Yang <yaoyu.yang88@gmail.com>
2014-03-17 15:34:29 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,9 +5,15 @@ google.setOnLoadCallback(drawMarkersMap);
  var data = google.visualization.arrayToDataTable([
    ['City',   'Duration'],
    ['Yuanping', 6],
-   ['Taiyuan',1],
-   ['Shanghai',4],
-   ['Beijing',0.2]
+   ['Taiyuan', 1],
+   ['Shanghai', 4],
+   ['Beijing', 0.2],
+   ['Dalian', 0.1],
+   ['Shenyang',0.1],
+   ['Shijiazhuang',0.1],
+   ['Jinan',0.1],
+   ['Taian',0.1],
+   ['Yinchuan',0.1]
  ]);
 
  var options = {
