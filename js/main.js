fd09eba9955cad1642075d3b1d5f7b81149ced5f
Yaoyu Yang <yaoyu.yang88@gmail.com>
2014-03-17 16:00:34 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,13 +7,22 @@ google.setOnLoadCallback(drawMarkersMap);
    ['Yuanping', 6],
    ['Taiyuan',1],
    ['Shanghai',4],
-   ['Beijing',0.2]
+   ['Beijing',0.2],
+   ['Dalian', 0.1],
+   ['Shenyang',0.1],
+   ['Shijiazhuang',0.1],
+   ['Jinan',0.1],
+   ['Taian',0.1],
+   ['Yinchuan',0.1],
+   ['Lanzhou',0.1],
+   ['Dunhuang',0.1],
+   ['Changsha',0.1]
  ]);
 
  var options = {
    region: 'CN',
    displayMode: 'markers',
-   colorAxis: {colors: ['green','blue']}
+   colorAxis: {colors: ['#e7711c', '#4374e0']}
  };
 
  var chart = new google.visualization.GeoChart(document.getElementById('chart_div'));
