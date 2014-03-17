bf74c11fe65f7c4a282e9903243e36e8df156cf3
Yaoyu Yang <yaoyu.yang88@gmail.com>
2014-03-17 15:19:53 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,22 +3,15 @@
 
       function drawMarkersMap() {
       var data = google.visualization.arrayToDataTable([
-        ['City',   'Population', 'Area'],
-        ['Rome',      2761477,    1285.31],
-        ['Milan',     1324110,    181.76],
-        ['Naples',    959574,     117.27],
-        ['Turin',     907563,     130.17],
-        ['Palermo',   655875,     158.9],
-        ['Genoa',     607906,     243.60],
-        ['Bologna',   380181,     140.7],
-        ['Florence',  371282,     102.41],
-        ['Fiumicino', 67370,      213.44],
-        ['Anzio',     52192,      43.43],
-        ['Ciampino',  38262,      11]
+        ['City',   'Time'],
+        ['Yuanping', 'Hometown'],
+        ['Taiyuan','2006-2007'],
+        ['Shanghai','2007-2011'],
+        ['Beijing','2008,2009,2011,2012,2013']
       ]);
 
       var options = {
-        region: 'IT',
+        region: 'CN',
         displayMode: 'markers',
         colorAxis: {colors: ['green', 'blue']}
       };
