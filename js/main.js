3aa001fd77511c3eca90344b16785d65d5979c9c
Yaoyu Yang <yaoyu.yang88@gmail.com>
2014-03-17 15:16:30 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,28 @@
+     google.load('visualization', '1', {'packages': ['geochart']});
+     google.setOnLoadCallback(drawMarkersMap);
 
-window.onload=function() {
-  
-  var elems = document.getElementsByClassName('paper');
-  var n = elems.length;
-  for (i in elems) {
-    elems[i].innerHTML = "<span class='num'>[" + n + "]</span> " + elems[i].innerHTML;
-    n--;
-  }
- 
-}
+      function drawMarkersMap() {
+      var data = google.visualization.arrayToDataTable([
+        ['City',   'Population', 'Area'],
+        ['Rome',      2761477,    1285.31],
+        ['Milan',     1324110,    181.76],
+        ['Naples',    959574,     117.27],
+        ['Turin',     907563,     130.17],
+        ['Palermo',   655875,     158.9],
+        ['Genoa',     607906,     243.60],
+        ['Bologna',   380181,     140.7],
+        ['Florence',  371282,     102.41],
+        ['Fiumicino', 67370,      213.44],
+        ['Anzio',     52192,      43.43],
+        ['Ciampino',  38262,      11]
+      ]);
 
+      var options = {
+        region: 'IT',
+        displayMode: 'markers',
+        colorAxis: {colors: ['green', 'blue']}
+      };
+
+      var chart = new google.visualization.GeoChart(document.getElementById('chart_div'));
+      chart.draw(data, options);
+    };
