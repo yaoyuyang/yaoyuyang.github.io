100be412badd824e19ee444f1a3d75b044df5b19
Yaoyu Yang <yaoyu.yang88@gmail.com>
2014-03-17 15:54:30 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,25 +3,11 @@ google.setOnLoadCallback(drawMarkersMap);
 
  function drawMarkersMap() {
  var data = google.visualization.arrayToDataTable([
-<<<<<<< HEAD
-   ['City',    'Details'],
-   ['Yuanping', 'Yuanping\rMiddle and high school here'],
-   ['Taiyuan', 'Taiyuan\nStayed a year after high school graduation'],
-   ['Shanghai', 'Collge study at SJTU'],
-   ['Beijing', 'Visited several times'],
-   ['Dalian', 'A beautiful city'],
-   ['Shenyang','Visited while in college'],
-   ['Shijiazhuang','best friend\'s hometown'],
-   ['Jinan','Baotuquan'],
-   ['Taian','The gorgeous moutain'],
-   ['Yinchuan','A northwest perl']
-=======
    ['City',   'Duration'],
    ['Yuanping', 6],
    ['Taiyuan',1],
    ['Shanghai',4],
    ['Beijing',0.2]
->>>>>>> parent of c09a612... Update main.js
  ]);
 
  var options = {
