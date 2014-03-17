ae6f01a5d0b3f8870f96f46ece2456fc5c249dc0
Yaoyu Yang <yaoyu.yang88@gmail.com>
2014-03-17 16:04:47 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,20 +3,26 @@ google.setOnLoadCallback(drawMarkersMap);
 
  function drawMarkersMap() {
  var data = google.visualization.arrayToDataTable([
-   ['City',   'Duration'],
+   ['City',  'Duration'],
    ['Yuanping', 6],
    ['Taiyuan',1],
    ['Shanghai',4],
    ['Beijing',0.2],
    ['Dalian', 0.1],
    ['Shenyang',0.1],
-   ['Shijiazhuang',0.1],
-   ['Jinan',0.1],
-   ['Taian',0.1],
-   ['Yinchuan',0.1],
-   ['Lanzhou',0.1],
-   ['Dunhuang',0.1],
-   ['Changsha',0.1]
+   ['Shijiazhuang', 0.1],
+   ['Hangzhou', 0.1],
+   ['Ningbo', 0.1],
+   ['Suzhou', 0.1],
+   ['Nanjing', 0.1],
+   ['Jinan', 0.1],
+   ['Taian', 0.1],
+   ['Qufu', 0.1],
+   ['Yinchuan', 0.1],
+   ['Lanzhou', 0.1],
+   ['Dunhuang', 0.1],
+   ['Jiayuguan', 0.1],
+   ['Changsha', 0.1]
  ]);
 
  var options = {
