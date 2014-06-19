3786be4f972d60e076bf0ff3894be8fe93e04f7b
sunshineyyy <yaoyu.yang88@gmail.com>
2014-06-18 23:58:37 -0700

    minimal working structure

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,19 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def education
+  end
+
+  def research
+  end
+
+  def travel
+  end
+
+  def more
+  end
+
+  def about
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Yaoyuyang::Application.routes.draw do
+  root 'static_pages#home'
+  match '/about',    to: 'static_pages#about',  via: 'get'
+  match '/education', to: 'static_pages#education', via: 'get'
+  match '/research',  to: 'static_pages#research', via: 'get'
+  match '/travel',    to: 'static_pages#travel',  via: 'get'
+  match '/more',      to: 'static_pages#more',    via: 'get'
+  match '/contact',   to: 'static_pages#contact', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
