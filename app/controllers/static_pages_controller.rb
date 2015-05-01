dd7434c13d948f562127aa48ac1809ecd19e6c0d
sunshineyyy <yaoyu.yang88@gmail.com>
2015-04-30 20:47:31 -0700

    add copyright

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,24 @@
 class StaticPagesController < ApplicationController
+
   def home
+    @klavinslab = "http://klavinslab.org/"
+    @uw = "http://www.washington.edu/"
+    @uwee = "http://www.ee.washington.edu/"
+    @sjtu = "http://en.sjtu.edu.cn/"
+    @auto_sjtu = "http://www.automation.sjtu.edu.cn/en/"
+    @control_theory = "http://en.wikipedia.org/wiki/Control_theory"
+    @automation_wiki = "http://en.wikipedia.org/wiki/Automation"
+    @syntheti_biology = "http://www.synberc.org/what-is-synbio"
   end
 
   def education
   end
 
   def projects
+    @klavinslab = "http://klavinslab.org/"
+    @nemlab = "http://depts.washington.edu/nemlab/"
+    @aquarium = "http://klavinslab.org/aquarium.html"
+    @bode_plot = "http://en.wikipedia.org/wiki/Bode_plot"
   end
 
   def more
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,4 @@
 module ApplicationHelper
-
   # Returns the full title on a per-page basis
   def full_title(page_title)
     base_title = "Yaoyu Yang"
@@ -9,4 +8,25 @@ module ApplicationHelper
       "#{base_title} | #{page_title}"
     end
   end
+
+  def copyright_notice_year_range(start_year)
+    # In case the input was not a number (nil.to_i will return a 0)
+    start_year = start_year.to_i
+
+    # Get the current year from the system
+    current_year = Time.new.year
+
+    # When the current year is more recent than the start year, return a string 
+    # of a range (e.g., 2010 - 2012). Alternatively, as long as the start year 
+    # is reasonable, return it as a string. Otherwise, return the current year 
+    # from the system.
+    if current_year > start_year && start_year > 2000
+      "#{start_year} - #{current_year}"
+    elsif start_year > 2000
+      "#{start_year}"
+    else
+      "#{current_year}"
+    end
+  end
+  
 end
